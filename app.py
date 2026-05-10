7769a588ec8ae3c1e45a8ba7e392a8660128929b
Shivaraj <shivarajpm57@gmail.com>
2026-05-10 20:27:40 +0530

    added image upload

--- app.py
+++ app.py
@@ -1,44 +1,30 @@
-from flask import Flask, render_template, Response
-import cv2
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
-
-def generate_frames():
-
-    while True:
-
-        success, frame = camera.read()
-
-        if not success:
-            break
-
-        else:
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-
-            frame = buffer.tobytes()
-
-            yield (
-                b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' +
-                frame +
-                b'\r\n'
-            )
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
 
-    return render_template("index.html")
+    filename = None
 
-@app.route('/video')
-def video():
+    if request.method == 'POST':
 
-    return Response(
-        generate_frames(),
-        mimetype='multipart/x-mixed-replace; boundary=frame'
+        image = request.files['image']
+
+        if image:
+
+            os.makedirs("static/uploads", exist_ok=True)
+
+            filepath = os.path.join("static/uploads", image.filename)
+
+            image.save(filepath)
+
+            filename = image.filename
+
+    return render_template(
+        "index.html",
+        filename=filename
     )
 
 if __name__ == '__main__':
